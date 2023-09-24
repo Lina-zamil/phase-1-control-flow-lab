13c760da6d56f0ba50c792e012df84a7966f461b
lina abumahfood <lina.abumahfood@gmail.com>
2023-09-25 00:24:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
   // Write your code here!
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if (400 < ride && ride <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (2000 < ride && ride <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (ride > 2500) {
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let yes = "Ok, sounds good.";
+  let no = "No go.";
+  return (city === "NYC" ? yes : no);
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+      break;
+  }
 }
